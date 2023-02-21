fb8189450c2403923289eb5646a5b1d84472e7ad
gabewolford <gabewolford@gmail.com>
2023-02-21 07:06:31 -0800

    updated styles-light.css and started building dark mode functionality

--- javascript/app.js
+++ javascript/app.js
@@ -2,12 +2,23 @@ const toggleSwitch = document.querySelector('#toggle-button');
 console.log(toggleSwitch);
 const stylesheet = document.querySelector('[href="/css/styles-light.css"]');
 console.log(stylesheet);
-const blurb = document.querySelector('src="/assets/blurb-photo.png"]')
+const blurb = document.querySelector('img[src="/assets/blurb-photo.png"]');
+console.log(blurb);
 
 toggleSwitch.addEventListener('change', () => {
     if (toggleSwitch.checked) {
         stylesheet.href = '/css/styles-dark.css';
-        blurb.src = '/assets/blurb-photo.png';
+        blurb.src = '/assets/blurb-photo-dark.png';
+    } else {
+        stylesheet.href = '/css/styles-light.css';
+    }
+});
+
+toggleSwitch.addEventListener('change', () => {
+    if (toggleSwitch.checked) {
+        const home = document.querySelector(".home");
+        console.log(home)
+        home.innerHTML = 'ET phone home'
     } else {
         stylesheet.href = '/css/styles-light.css';
     }
