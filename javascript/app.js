9f1d4bbe26216d2fc6f292c1270d4e5272f81af8
gabewolford <gabewolford@gmail.com>
2023-02-21 19:04:40 -0800

    added hover state photos, started rebuilding mobile first

--- javascript/app.js
+++ javascript/app.js
@@ -1,25 +1,20 @@
-const toggleSwitch = document.querySelector('#toggle-button');
+// DARK MODE MESS
+const toggleSwitch = document.querySelector('.slider');
 console.log(toggleSwitch);
-const stylesheet = document.querySelector('[href="/css/styles-light.css"]');
-console.log(stylesheet);
-const blurb = document.querySelector('img[src="/assets/blurb-photo.png"]');
-console.log(blurb);
+const emailIcon = document.querySelector('#email');
+console.log(emailIcon);
+const linkedinIcon = document.querySelector('#linkedin');
+console.log(linkedinIcon);
+const githubIcon = document.querySelector('#github');
+console.log(githubIcon);
 
-toggleSwitch.addEventListener('change', () => {
-    if (toggleSwitch.checked) {
-        stylesheet.href = '/css/styles-dark.css';
-        blurb.src = '/assets/blurb-photo-dark.png';
-    } else {
-        stylesheet.href = '/css/styles-light.css';
-    }
-});
 
-toggleSwitch.addEventListener('change', () => {
-    if (toggleSwitch.checked) {
-        const home = document.querySelector(".home");
-        console.log(home)
-        home.innerHTML = 'ET phone home'
-    } else {
-        stylesheet.href = '/css/styles-light.css';
-    }
-});
+function changeToDark() {
+    document.body.style.backgroundColor = "var(--black)"
+    document.body.style.color = "var(--white)"
+    emailIcon.src = '/assets/email-circle-dark.png'
+    linkedinIcon.src = '/assets/linkedin-circle-dark.png'
+    githubIcon.src = '/assets/github-circle-dark.png'
+}
+
+toggleSwitch.addEventListener('click', changeToDark);
