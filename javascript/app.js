699b18d8a1d0b55f9cebf780504c488ce6fd90ee
gabewolford <gabewolford@gmail.com>
2023-02-22 21:44:18 -0800

    finishing touches made, borders removed

--- javascript/app.js
+++ javascript/app.js
@@ -22,41 +22,24 @@ const darkModeToggle = document.querySelector('.slider');
 const toggleSwitch = document.querySelector('.switch input[type="checkbox"]');
 const navItems = document.querySelectorAll('.nav-item');
 const introBlurb = document.querySelector('.h1-text');
-// const emailBug = document.querySelector('#github');
-// const linkedInBug = document.querySelector('#linkedin');
-// const githubBug = document.querySelector('#github');
-// console.log(emailBug);
-// console.log(linkedInBug);
-// console.log(githubBug);
+const linkDefault = document.querySelectorAll('.link-default');
 
 toggleSwitch.addEventListener('change', switchTheme, false);
 
 
 function switchTheme(event) {
-    let emailBug = document.querySelector('#github');
-    console.log(emailBug);
-    let linkedInBug = document.querySelector('#linkedin');
-    console.log(linkedInBug);
-    let githubBug = document.querySelector('#github');
-    console.log(githubBug);
     if (event.target.checked) {
         // dark mode is on
         navItems.forEach(item => item.classList.add('dark-mode-nav'));
         introImage.src = '/assets/mobile blurb dark.png';
-        introBlurb.src = '/assets/default desktop blurb dark.png';
+        introBlurb.src = '/assets/hover desktop blurb dark.png';
         hamburgerImage.src = '/assets/white-hamburger.png';
-        emailBug.src = '/assets/email-circle.png';
-        linkedInBug.src = '/assets/linkedin-circle.png';
-        githubBug.src ='/assets/github-circle.png';
     } else {
         // dark mode is off
         navItems.forEach(item => item.classList.remove('dark-mode-nav'));
         introImage.src = '/assets/mobile blurb light.png';
-        introBlurb.src = '/assets/default desktop blurb light.png';
+        introBlurb.src = '/assets/hover desktop blurb light.png';
         hamburgerImage.src = '/assets/black-hamburger.png'; 
-        emailBug.src = '/assets/email-circle.png';
-        linkedInBug.src = '/assets/linkedin-circle.png';
-        githubBug.src = '/assets/github-circle.png';
     }
 }
 
@@ -74,8 +57,6 @@ darkModeToggle.addEventListener('click', darkMode);
 
 
 // link hover effects #3
-const linkDefault = document.querySelectorAll('.link-default');
-
 linkDefault.forEach(linkDefault => {
     const originalImg = linkDefault.querySelector('img').src;
     const newImg = '/assets/hover left arrow check it out.png';
