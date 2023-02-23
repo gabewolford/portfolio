9d678842b8c039ebe1e32852d4773ab7be91bf00
gabewolford <gabewolford@gmail.com>
2023-02-22 20:16:23 -0800

    fixed darkmode bug, added new color icons

--- javascript/app.js
+++ javascript/app.js
@@ -16,27 +16,47 @@ closeIcon.addEventListener('click', () => {
 })
 
 // dark mode functionality #2
-const introImage = document.querySelector('.intro-image')
+const introImage = document.querySelector('.intro-image');
 const hamburgerImage = document.querySelector('.hamburger-image');
-const darkModeToggle = document.querySelector('.slider')
+const darkModeToggle = document.querySelector('.slider');
 const toggleSwitch = document.querySelector('.switch input[type="checkbox"]');
 const navItems = document.querySelectorAll('.nav-item');
-console.log(navItems);
+const introBlurb = document.querySelector('.h1-text');
+// const emailBug = document.querySelector('#github');
+// const linkedInBug = document.querySelector('#linkedin');
+// const githubBug = document.querySelector('#github');
+// console.log(emailBug);
+// console.log(linkedInBug);
+// console.log(githubBug);
 
-console.log(toggleSwitch);
 toggleSwitch.addEventListener('change', switchTheme, false);
 
+
 function switchTheme(event) {
+    let emailBug = document.querySelector('#github');
+    console.log(emailBug);
+    let linkedInBug = document.querySelector('#linkedin');
+    console.log(linkedInBug);
+    let githubBug = document.querySelector('#github');
+    console.log(githubBug);
     if (event.target.checked) {
         // dark mode is on
         navItems.forEach(item => item.classList.add('dark-mode-nav'));
         introImage.src = '/assets/mobile blurb dark.png';
+        introBlurb.src = '/assets/default desktop blurb dark.png';
         hamburgerImage.src = '/assets/white-hamburger.png';
+        emailBug.src = '/assets/email-circle.png';
+        linkedInBug.src = '/assets/linkedin-circle.png';
+        githubBug.src ='/assets/github-circle.png';
     } else {
         // dark mode is off
         navItems.forEach(item => item.classList.remove('dark-mode-nav'));
-        introImage.src = '/assets/mobile blurb light.png'
+        introImage.src = '/assets/mobile blurb light.png';
+        introBlurb.src = '/assets/default desktop blurb light.png';
         hamburgerImage.src = '/assets/black-hamburger.png'; 
+        emailBug.src = '/assets/email-circle.png';
+        linkedInBug.src = '/assets/linkedin-circle.png';
+        githubBug.src = '/assets/github-circle.png';
     }
 }
 
@@ -58,7 +78,7 @@ const linkDefault = document.querySelectorAll('.link-default');
 
 linkDefault.forEach(linkDefault => {
     const originalImg = linkDefault.querySelector('img').src;
-    const newImg = '/assets/hover check it out right arrow.png';
+    const newImg = '/assets/hover left arrow check it out.png';
 
     linkDefault.addEventListener('mouseenter', () => {
         linkDefault.querySelector('img').src = newImg;
