fce9db99734d5aaa08c7c97aaf52833ed2d46f0b
gabewolford <gabewolford@gmail.com>
2023-02-24 16:09:43 -0800

    MVP

--- app.js
+++ app.js
@@ -28,20 +28,20 @@ function switchTheme() {
         // dark mode is on
         navItems.forEach(item => item.classList.add('dark-mode-nav'));
         document.body.classList.add('dark-mode');
-        if (location.pathname === '/' || location.pathname === '/portfolio/index.html') {
-            introImage.src = '../assets/mobile blurb dark.png'; 
-            introBlurb.src = '../assets/hover desktop blurb dark.png'; 
+        if (location.pathname === '/' || location.pathname === '/index.html') {
+            introImage.src = 'assets/mobile blurb dark.png'; 
+            introBlurb.src = 'assets/hover desktop blurb dark.png'; 
         }
-        hamburgerImage.src = '../assets/white-hamburger.png';
+        hamburgerImage.src = 'assets/white-hamburger.png';
     } else {
         // dark mode is off
         navItems.forEach(item => item.classList.remove('dark-mode-nav'));
         document.body.classList.remove('dark-mode');
-        if (location.pathname === '/' || location.pathname === '/portfolio/index.html') {
-            introImage.src = '../assets/mobile blurb light.png';
-            introBlurb.src = '../assets/hover desktop blurb light.png';
+        if (location.pathname === '/' || location.pathname === '/index.html') {
+            introImage.src = 'assets/mobile blurb light.png';
+            introBlurb.src = 'assets/hover desktop blurb light.png';
         }
-        hamburgerImage.src = '../assets/black-hamburger.png';
+        hamburgerImage.src = 'assets/black-hamburger.png';
     }
 }
 
