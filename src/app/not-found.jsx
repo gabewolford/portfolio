062160f49310fef2fa2809810460d8a35621f32a
gabewolford <gabewolford@gmail.com>
2023-07-21 14:41:31 -0700

    made not found page

---
+++ src/app/not-found.jsx
@@ -0,0 +1,12 @@
+export default function NotFound() {
+  return (
+    <h1 className="grid h-screen place-items-center text-3xl mx-auto md:text-4xl ml-10 md:mx-12 animate__animated animate__fadeIn">
+        <span>
+            <span className="text-pink hover:text-green transition ease-in-out duration-500">Page </span>
+            <span className="text-dark-grey hover:text-blue transition ease-in-out duration-500">Not </span>
+            <span className="text-green hover:text-yellow transition ease-in-out duration-400">Found</span>
+        </span>
+        
+    </h1>
+  )
+}
