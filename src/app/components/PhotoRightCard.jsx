b469bfc9861a755aafe45e562adc75acb5eb3ebd
gabewolford <gabewolford@gmail.com>
2023-08-28 09:40:29 -0700

    moved projects into componenets

---
+++ src/app/components/PhotoRightCard.jsx
@@ -0,0 +1,28 @@
+import Link from "next/link"
+
+export default function PhotoRightCard({ imageUrl, altText, githubUrl, siteUrl, projectName, monthAndYear, technologies, ctaText }) {
+    return (
+        <section className="fade-in show flex flex-col md:flex-row items-center gap-6 md:gap-14 w-full">
+            <div className="flex flex-col md:ml-18 md:w-1/2">
+                <Link href={githubUrl} target="_blank">
+                    <h4 className="text-2xl md:text-3xl hover:text-orange font-md leading-7 mb-1 transition ease-in-out duration-300">{projectName}</h4>
+                </Link>
+                <p className="text-xs mb-2 text-dark-grey">{monthAndYear}</p>
+            
+                <p className="text-sm mb-2">{technologies}</p>
+                <div className="flex items-center">
+                    <Link href={siteUrl} target="_blank">
+                        <span className="text-blue text-md hover:text-pink hover:underline hover:underline-offset-8 hover:decoration-2">{ctaText}</span>
+                    </Link>
+                    <Link href={siteUrl} target="_blank">
+                        <img className="w-6 ml-2" src='/images/right-arrow.svg' alt='right arrow'/>
+                    </Link>
+                </div>
+            </div>
+            
+            <div className='order-last max-h-96 overflow-auto scrollbar-hide md:w-1/2'>
+                <img src={imageUrl} alt={altText} />
+            </div>
+        </section>
+    )
+  }
