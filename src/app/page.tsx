5f9e398c970ae3adab03109e21a0363b99bb746d
gabewolford <gabewolford@gmail.com>
2023-11-20 10:45:52 -0800

    updated resume

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,11 @@ export default function HomePage() {
           <span>i&apos;m a </span>
           <span className="text-yellow hover:text-blue">&#123;</span>
           <span className="text-brown hover:text-green transition ease-in-out duration-500">
-            fullstack
+            ui
+          </span>
+          <span className="text-dark-grey">.</span>
+          <span className="text-brown hover:text-green transition ease-in-out duration-500">
+            ux
           </span>
           <span className="text-dark-grey">.</span>
           <span className="text-orange hover:text-pink transition ease-in-out duration-500">
